6811a09989ae57ce9fc30093ff5226b9b24b71ab
JubaDeveloper <michaelres1332@gmail.com>
2023-09-17 15:00:55 -0300

    Added employee service

--- src/main/java/org/jubadeveloper/service/EmployeeService.java
+++ src/main/java/org/jubadeveloper/service/EmployeeService.java
@@ -1,2 +1,45 @@
-package org.jubadeveloper.service;public class EmployeeService {
+package org.jubadeveloper.service;
+
+import org.jubadeveloper.entitie.Employee;
+import org.jubadeveloper.respository.EmployeeRepository;
+import org.jubadeveloper.service.models.EmployeeServiceModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.jubadeveloper.service.error.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService implements EmployeeServiceModel {
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> fetch() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee create(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee get(Long id) throws EmployeeNotFound {
+        return employeeRepository.findById(id).orElseThrow(() -> new EmployeeNotFound(id));
+    }
+
+    @Override
+    public Employee delete(Employee employee) throws EmployeeNotFound {
+        return employeeRepository.findById(employee.getId()).map(employee1 -> {
+            employeeRepository.delete(employee1);
+            return employee1;
+        }).orElseThrow(() -> new EmployeeNotFound(employee.getId()));
+    }
+
+    @Override
+    public Employee update(Long id, Employee newEmployee) {
+        return null;
+    }
 }
