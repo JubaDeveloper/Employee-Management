aca455a5237dbd86b264c70d8177c5d8b25911a6
JubaDeveloper <michaelres1332@gmail.com>
2023-09-17 14:58:11 -0300

    Created Database loader config

--- src/main/java/org/jubadeveloper/config/LoadDatabase.java
+++ src/main/java/org/jubadeveloper/config/LoadDatabase.java
@@ -1,2 +1,26 @@
-package org.jubadeveloper.config;public class LoadDatabase {
+package org.jubadeveloper.config;
+
+import org.jubadeveloper.entitie.Employee;
+import org.jubadeveloper.entitie.EmployeeStatus;
+import org.jubadeveloper.service.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class LoadDatabase {
+    @Autowired
+    private EmployeeService employeeService;
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase () {
+        return args -> {
+            log.info("Preloading " + employeeService.create(new Employee("Juan", EmployeeStatus.ACTIVE)));
+            log.info("Preloading " + employeeService.create(new Employee("Marquito", EmployeeStatus.ACTIVE)));
+        };
+    }
 }
